45b22f34fe94aa1a40c6d5757b0cebb96ac652ea
Aurea <fu.sakura@gmail.com>
2018-08-14 12:08:57 -0700

    completing blackjack_score

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -1,7 +1,31 @@
 # blackjack_score.rb
 
-VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'King', 'Queen', 'Jack']
+VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'King', 'Queen', 'Jack', 'Ace']
 
 def blackjack_score(hand)
 
+  score = 0
+  hand.each do |card|
+
+    unless VALID_CARDS.includes?(card)
+      raise ArgumentError, 'Invalid card'
+    end
+
+    if card == 'King' || card == 'Queen' || card == 'Jack'
+      score += 10
+
+    elsif card == 'Ace'
+      # Add 11 if possible, otherwise add 1
+      if score + 11 > 21
+        score += 1
+      else
+        score += 11
+      end
+
+    else
+      score += card
+    end
+  end
+
+  return score
 end
--- specs/blackjack_score_spec.rb
+++ specs/blackjack_score_spec.rb
@@ -8,7 +8,7 @@ require_relative '../lib/blackjack_score'
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-describe 'Blackjac Score' do
+describe 'Blackjack Score' do
   it 'can calculate the score for a pair of number cards' do
 
     # Arrange
@@ -18,19 +18,33 @@ describe 'Blackjac Score' do
     score = blackjack_score(hand)
 
     # Assert <-  You do this part!
-
+    expect ( score ).must_equal(7)
   end
 
   it 'facecards have values calculated correctly' do
+      hand = [3, 5, 'King']
+
+      score = blackjack_score(hand)
+
+      expect ( score ).must_equal(18)
 
   end
 
   it 'calculates aces as 11 where it does not go over 21' do
+      hand = ['Ace', 5, 1, 'Ace']
+
+      score = blackjack_score(hand)
+
+      expect ( score ).must_equal(18)
 
   end
 
   it 'calculates aces as 1, if an 11 would cause the score to go over 21' do
+      hand = ['Ace', 'King', 9, 'Ace']
 
+      score = blackjack_score(hand)
+
+      expect ( score ).must_equal(31)
   end
 
   it 'raises an ArgumentError for invalid cards' do
